39cc4b4e9c959f55fb1559ac9e15f0f94f463194
arXaik <araik200414@gmail.com>
2022-12-21 19:32:54 +0200

    Добавьте файлы проекта.

---
+++ 3.2/Program.cs
@@ -0,0 +1,10 @@
+﻿//Петросян 16 вар Средний уровень
+Console.Write("Введите число n от 1 до 1000:");
+int n = int.Parse(Console.ReadLine());
+while (n > 1 && n < 1000)
+{
+    Console.Write("Введите число m:");
+    int m = int.Parse(Console.ReadLine());
+    int div = (n / m) * (n / m);
+    Console.WriteLine($"Квадрат целочисленного деления:{div}");
+}
